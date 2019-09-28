e392844cbf770f58a0151c5f87e10d5333ceda9f
ftc11697 <55903766+ftc11697@users.noreply.github.com>
2019-09-28 01:13:08 -0400

    Add files via upload
    New files.

---
+++ Hardware.java
@@ -0,0 +1,162 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorREVColorDistance;
+
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ * In this case that robot is a Pushbot.
+ * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
+ *
+ ***  Hardware Motor Controllers Settings ***
+ RL Controller (AL025T7Z)
+ 0 - NeveRest 40 Gearmotor (FLMotor)
+ RR Controller (AL025T80)
+ 1 - NeveRest 40 Gearmotor (FRMotor)
+ FL Controller (A7008KTV)
+ 2 - NeveRest 40 Gearmotor (LRMotor)
+ FR Controller (A7008KBB)
+ 3 - NeveRest 40 Gearmotor (RRMotor)
+ */
+
+public class Hardware
+
+    /**Unnecessary values are left as examples -- DON'T DELETE */
+
+{
+    //Define values for servos
+    public static final double stabbyIniOpen = 0.00;
+
+
+    /* Public OpMode members. */
+    public DcMotor frontLeftMotor      = null;     // H2 channel 0     FLMotor
+    public DcMotor frontRightMotor     = null;     // H2 channel 1     FRMotor
+    public DcMotor rearLeftMotor       = null;     // H2 channel 2     RLMotor
+    public DcMotor rearRightMotor      = null;     // H2 channel 3     RRMotor
+    public DcMotor horizontalMotor     = null;     // H3 channel 0     HorizMotor
+    public DcMotor verticalMotor       = null;     // H3 channel 1     VertMotor
+
+
+
+    /* Public Servos */
+    public Servo markyBoi              = null;     // H3 servo 1        markyBoi
+    public Servo hookyBoi              = null;     // H2 servo 0        hookyBoi
+    public Servo clampyBoi             = null;     // H2 servo 1        clampyBoi
+
+
+    /* local OpMode members. */
+    HardwareMap hwMap             =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public Hardware(){
+
+    }
+
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+
+        // Define and Initialize Motors
+        frontLeftMotor  = hwMap.dcMotor.get("LF_Motor");
+        frontRightMotor = hwMap.dcMotor.get("RF_Motor");
+        rearLeftMotor   = hwMap.dcMotor.get("LR_Motor");
+        rearRightMotor  = hwMap.dcMotor.get("RR_Motor");
+
+
+        // Define and initialize servos
+        markyBoi        = hwMap.get(Servo.class, "markyBoi");
+
+        //Initialize servo positions (DEFINE LATER)
+        markyBoi.setPosition(0.08);
+
+
+Original:
+
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        rearLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        rearRightMotor.setDirection(DcMotor.Direction.FORWARD);
+
+/*
+        frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
+        rearLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        rearRightMotor.setDirection(DcMotor.Direction.REVERSE);
+*/
+
+
+
+/*
+// Fixing inverted controls:
+
+        frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
+        rearLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        rearRightMotor.setDirection(DcMotor.Direction.REVERSE);
+  */
+// End of fix
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rearLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rearRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rearLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rearRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        frontLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        rearLeftMotor.setPower(0);
+        rearRightMotor.setPower(0);
+
+    }
+
+    /***
+     *
+     * waitForTick implements a periodic delay. However, this acts like a metronome with a regular
+     * periodic tick.  This is used to compensate for varying processing times for each cycle.
+     * The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
+     *
+     * @param periodMs  Length of wait cycle in mSec.
+     */
+    public void waitForTick(long periodMs) {
+
+        long  remaining = periodMs - (long)period.milliseconds();
+
+        // sleep for the remaining portion of the regular cycle period.
+        if (remaining > 0) {
+            try {
+                Thread.sleep(remaining);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        // Reset the cycle clock for the next pass.
+        period.reset();
+    }
+
+
+}
+
---
+++ RoboDrive.java
@@ -0,0 +1,200 @@
+/*
+https://github.com/Rambotics/FTC-2016-2017-v2.4-pc/tree/master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode
+https://github.com/pmtischler/ftc_app/tree/master/SharedCode/src/main/java/com/github/pmtischler
+
+Code Folding Expand/Collapse ALL => Control || Shift || +/-
+Last Edit Location => Control + Shift + BackSpace
+Add Bookmark => Control + F11 + number
+Find Bookmark => Control + number
+Show Bookmarks => Shift + F11
+Jump to Declaration => Control + B
+
+
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.RobotLog;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+//===========================================
+// ***** Section 1          *****
+// ***** Change OpMode Name *****
+//===========================================
+
+@TeleOp(name="RoboDrive_MS", group="11697")
+// @Disabled
+
+public class RoboDrive extends LinearOpMode {
+
+//===========================================
+// ***** Section 2                *****
+// ***** Declare global variables *****
+//===========================================
+
+    Hardware robot = new Hardware();
+
+    // Motors Settings [DO NOT CHANGE]
+    static final double     COUNTS_PER_MOTOR_REV    = 1120 ;    // eg: TETRIX Motor Encoder 1440, Andymark = 1120
+    static final double     DRIVE_GEAR_REDUCTION    = 1.3 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+
+    float                   SPEED_RATE           = 1.0f;       // Driving speed rate
+    double                  curPosition = 0.2;
+
+    /* Declare Global Variables. */
+    private Date today = new Date();
+    private DateFormat myDateFormat = new SimpleDateFormat("yy/MM/dd HH:mm:ss");
+
+    /* Insert more stuff as needed
+    *  Look at https://tinyurl.com/yb2xco82 for examples */
+
+
+/***** Main Code *****/
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+
+        robot.frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        robot.frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
+        robot.rearLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        robot.rearRightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+
+        logMessage("Status", "Initialized v4.0a - " +  myDateFormat.format(today));
+
+
+        waitForStart(); // Wait for the game to start (driver presses PLAY)
+
+        String                     PREVIOUS_MSG        = "";        // MesLog
+        float lxValue = 0, lyValue = 0, rxValue = 0, ryValue = 0;
+
+        while (opModeIsActive()) {
+
+            // Drive the robot
+            lxValue = getJoystickValue(gamepad1.left_stick_x);
+            lyValue = getJoystickValue(gamepad1.left_stick_y);
+            rxValue = getJoystickValue(gamepad1.right_stick_x);
+            ryValue = getJoystickValue(gamepad1.right_stick_y);
+            driveByJoystick(lxValue, lyValue, rxValue);
+
+
+            /********************************************
+             *************GAMEPAD 1**********************
+             ********************************************/
+
+            //ADD BUTTON FUNCTIONALITY HERE LATER
+
+
+
+            /********************************************
+             *************GAMEPAD 2**********************
+             ********************************************/
+
+            //ADD BUTTON FUNCTIONALITY HERE LATER
+
+
+
+            idle(); // Always call idle() at the bottom of your while(opModeIsActive()) loop
+
+        }
+
+        logMessage("Done", "Stopped!");
+    }
+
+
+
+//===========================================
+// ***** Section 5              *****
+// ***** User Defined Functions *****
+//===========================================
+
+    private void driveByJoystick(double lxValue, double lyValue, double rxValue) {
+        double vD = Math.sqrt(Math.pow(lxValue, 2) + Math.pow(lyValue, 2));
+        //double thetaD = Math.atan2(lyValue, lxValue);
+        double thetaD = Math.atan2(lxValue, -lyValue);
+        double vTheta = rxValue;
+
+        // Convert desired motion to wheel power, with power clamping.
+        Mecanum.Wheels wheels = Mecanum.motionToWheels(vD, thetaD, vTheta);
+        robot.frontLeftMotor.setPower(wheels.frontLeft);
+        robot.frontRightMotor.setPower(wheels.frontRight);
+        robot.rearLeftMotor.setPower(wheels.backLeft);
+        robot.rearRightMotor.setPower(wheels.backRight);
+
+        //telemetry.addData("Status", "==> " + vD +", "+thetaD+", "+vTheta);
+        //telemetry.update();
+    }
+
+    private void stopRobot() {
+        robot.frontLeftMotor.setPower(0);
+        robot.frontRightMotor.setPower(0);
+        robot.rearLeftMotor.setPower(0);
+        robot.rearRightMotor.setPower(0);
+    }
+
+    private void logMessage (String myDescription, String myMessage) throws InterruptedException {
+
+            telemetry.addData(myDescription, myMessage);
+            telemetry.update();
+            RobotLog.d("11697CW - " + myDescription + " : " + myMessage);
+
+    }
+
+    private float getJoystickValue (float rawValue) {
+        //clip the power values so that it only goes from -1 to 1
+        rawValue = Range.clip(rawValue, -1, 1);
+
+        if (Math.abs(rawValue) < 0.1) {
+            return 0;
+        } else {
+            return rawValue * SPEED_RATE;
+        }
+    }
+
+//===========================================
+// ***** Section 6              *****
+// ***** Backup Un-used Code    *****
+//===========================================
+
+/********************************************
+
+            if (gamepad1.dpad_up) {
+                V_POSITION += INCREMENT ;
+                if (V_POSITION >= robot.VArmHigh ) {
+                    V_POSITION = robot.VArmHigh;
+                }
+                robot.vArm.setPosition(V_POSITION);
+                telemetry.addData("UP >> Servo Position", "%5.2f", V_POSITION);
+                telemetry.update();
+                //sleep(CYCLE_MS);
+                robot.waitForTick(CYCLE_MS);
+            }
+            if (gamepad1.dpad_down) {
+                V_POSITION -= INCREMENT ;
+                if (V_POSITION <= robot.VArmLow ) {
+                    V_POSITION = robot.VArmLow;
+                }
+                robot.vArm.setPosition(V_POSITION);
+                telemetry.addData("DOWN >> Servo Position", "%5.2f", V_POSITION);
+                telemetry.update();
+                //sleep(CYCLE_MS);
+                robot.waitForTick(CYCLE_MS);
+            }
+
+********************************************/
+
+
+
+
+
+}
+
